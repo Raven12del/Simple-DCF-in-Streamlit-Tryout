67d5cea65ed61568820aa0e79d5576c718146373
Raven12del <ravenverburgh@gmail.com>
2026-06-03 22:25:10 +0200

    Add files via upload

--- Simple_DCF.py
+++ Simple_DCF.py
@@ -9,6 +9,13 @@ st.title('DCF Model')
 top_output_slot = st.empty()
 col1, col2 = st.columns(2, gap="small")
 
+stock = st.text_input(label="Company of Interest (ticker):", value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible", icon=None, width="stretch", bind=None)
+#stock = [input("Company of Interest (ticker):")]
+df = pd.DataFrame(columns=["Stock", 'Beta', 'Marketcap'])
+
+treasury_bill = yf.Ticker("^IRX")
+current_rate = treasury_bill.info.get("regularMarketPrice")
+
 with col1:
     base_revenue = st.slider("base revenue", min_value=0, max_value=500, step=5, value=200)
     revenue_growth_input = st.slider("revenue growth rate (in %)", min_value=0, max_value=50, step=1, value=10)
@@ -25,6 +32,24 @@ with col2:
 time = []
 time.append(time_input)
         
+for i in stock:
+    ticker = yf.Ticker(i)
+    info = ticker.info
+    Beta = info.get('beta')
+    debt_to_equity = info.get('debtToEquity')
+    market_cap = info.get('marketCap')
+
+d_e = debt_to_equity/100
+debt = d_e *market_cap
+equity = market_cap-debt
+debt_weight = debt/market_cap
+equity_weight = equity/market_cap
+
+rf = 0.03
+market_return = 0.10
+capm = (current_rate/100) + Beta*(market_return - (current_rate/100))
+capm_percentage = round(capm*100, 4)
+
 for i in time:
     if i > 1:
         time.append(i-1)    
@@ -56,7 +81,7 @@ NOPAT = ebit*(1-tax_rate)
 FCF = NOPAT + d_a - capex - change_nwc  
 #for loop in range of input time to calculate PV of estimated FCF at that time period
 
-cost_of_equity = rf_rate + beta*(risk_premium)
+cost_of_equity = (current_rate/100) + beta*(risk_premium)
 cost_of_debt = cost_debt*(1-tax_rate)
 wacc = (1-debt_weight)*cost_of_equity + debt_weight*cost_of_debt
 
@@ -105,3 +130,6 @@ with col1:
     st.bar_chart(data=projected_rev, x=None, y=None, x_label="years", y_label="projected revenue", color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
 with col2:
     st.bar_chart(data=projected_CF, x=None, y=None, x_label="years", y_label="projected Cashflows", color=None, horizontal=False, sort=True, stack=None, width="stretch", height="content", use_container_width=None)
+
+st.header("The Calculated CAPM is:")
+st.text(capm_percentage)
